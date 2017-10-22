626d28533258f70099c320d53860182f864678ff
SealOfFire <sealoffire@hotmail.com>
2017-10-22 21:06:47 +0800

    角色动画
    角色动画

---
+++ HexagonMap/New Unity Project 2/Assets/Character/Character.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private Animator anim;
+
+    // Use this for initialization
+    private void Start()
+    {
+        this.anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // anim.SetBool("standing",false);
+            anim.SetInteger("manact", 1);
+            Debug.Log("GetKeyDown");
+        }
+        else if (Input.GetKeyUp(KeyCode.R))
+        {
+            anim.SetInteger("manact", 0);
+            Debug.Log("GetKeyUp");
+        }
+    }
+}
